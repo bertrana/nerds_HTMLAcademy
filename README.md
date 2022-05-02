a9856faef68b5f32f244b9785c6bafff668e44e8
bertrana <botediom@gmail.com>
2022-05-02 18:33:17 +0200

    added pins to the carousel, but no animation

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Nerds
 Сайт для дизайн-студии из Краснодара.
-[Быстрый просмотр](https://bertrana.github.io/nerds_HTMLAcademy)
 
+Просмотреть сайт можно по [ссылке](https://bertrana.github.io/nerds_HTMLAcademy).
+
+Вставить ссылки на макеты
+
+Быстрая инструкция по Markdown https://commonmark.org/help/
+Пример оформления проекта https://github.com/kottans/frontend 
--- js/modal.js
+++ js/modal.js
@@ -6,9 +6,9 @@ function closeModal() {
   modal.classList.add('visually-hidden');
 }
 
-let modal = document.querySelector('.modal');
-let openButton = document.querySelector('.modal-button');
-let closeButton = modal.querySelector('.modal-close');
+const modal = document.querySelector('.modal');
+const openButton = document.querySelector('.modal-button');
+const closeButton = modal.querySelector('.modal-close');
 
 openButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
